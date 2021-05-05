f7df01b197241542624aa777c4d38a99e21d4fb8
Waynee G Santos <waynee@outlook.com.br>
2021-05-05 09:13:38 -0300

    tulip: update vendorsetup
    Signed-off-by: Waynee G Santos <waynee@outlook.com.br>

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -3,3 +3,4 @@ export SELINUX_IGNORE_NEVERALLOWS=true
 export TEMPORARY_DISABLE_PATH_RESTRICTIONS=true
 export USE_CCACHE=1
 export CCACHE_EXEC=$(command -v ccache)
+cd kernel/xiaomi/tulip && git pull -q --force && cd ../../..
