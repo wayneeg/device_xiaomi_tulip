da7c4f0ef38dfa8b7606740b13d648784dc48446
wayneeg <waynee@outlook.com.br>
2021-05-11 15:06:46 -0300

    tenx bringup
    Signed-off-by: wayneeg <waynee@outlook.com.br>

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -14,9 +14,9 @@
 # limitations under the License.
 
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/derp_tulip.mk
+    $(LOCAL_DIR)/aosp_tulip.mk
 
 COMMON_LUNCH_CHOICES := \
-   derp_tulip-user \
-   derp_tulip-userdebug \
-   derp_tulip-eng
+   aosp_tulip-user \
+   aosp_tulip-userdebug \
+   aosp_tulip-eng
