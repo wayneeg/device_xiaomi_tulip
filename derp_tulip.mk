ca25b594d2d57b410379a2b25ec2ef9cff2c5420
Waynee G Santos <waynee@outlook.com.br>
2021-07-07 16:55:18 -0300

    tulip: update redfin fp to july
    Signed-off-by: Waynee G Santos <waynee@outlook.com.br>

--- derp_tulip.mk
+++ derp_tulip.mk
@@ -48,7 +48,7 @@ PRODUCT_BUILD_PROP_OVERRIDES += \
     PRIVATE_BUILD_DESC="redfin-user 11 210505.003 7255357 release-keys"
 
 #Build FP to be picked by both system and vendor
-BUILD_FINGERPRINT := "google/redfin/redfin:11/RQ3A.210605.005/7349499:user/release-keys"
+BUILD_FINGERPRINT := "google/redfin/redfin:11/RQ3A.210705.001/7380771:user/release-keys"
 
 PRODUCT_SYSTEM_DEFAULT_PROPERTIES += \
     ro.build.fingerprint=$(BUILD_FINGERPRINT)
