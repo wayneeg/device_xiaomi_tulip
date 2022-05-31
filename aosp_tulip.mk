98b1b4eb9a11f3c51d195b2da078d24f78c88c24
matheucomth <matheussouzas65@outlook.com>
2022-05-31 19:29:53 -0300

    tulip: Bump to latest raven fp

--- aosp_tulip.mk
+++ aosp_tulip.mk
@@ -30,6 +30,6 @@ TARGET_SUPPORTS_QUICK_TAP := true
 
 # Fingerprint
 PRODUCT_BUILD_PROP_OVERRIDES += \
-    PRIVATE_BUILD_DESC="raven-user 12 SP2A.220405.004 8233519 release-keys"
+    PRIVATE_BUILD_DESC="raven-user 12 SP2A.220505.002 8353555 release-keys"
 
-BUILD_FINGERPRINT := google/raven/raven:12/SP2A.220405.004/8233519:user/release-keys
+BUILD_FINGERPRINT := google/raven/raven:12/SP2A.220505.002/8353555:user/release-keys
