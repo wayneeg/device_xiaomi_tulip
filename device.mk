ccb02deeeacf2e509c6477725f548923abf30bb4
pranshoe <pranshu.goyal71@gmail.com>
2022-06-13 12:39:38 -0300

    tulip: Product Board Declaration

--- device.mk
+++ device.mk
@@ -17,6 +17,10 @@ $(call inherit-product, frameworks/native/build/phone-xhdpi-4096-dalvik-heap.mk)
 # Indicate the first api level the device has been commercially launched on
 PRODUCT_SHIPPING_API_LEVEL := 27
 
+# Platform
+PRODUCT_BOARD_PLATFORM := sdm660
+PRODUCT_USES_QCOM_HARDWARE := true
+
 # Overlays
 DEVICE_PACKAGE_OVERLAYS += \
     $(DEVICE_PATH)/overlay
