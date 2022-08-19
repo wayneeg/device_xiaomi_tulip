b071386e43f5ed4925cb05e45356dce3c519efb5
tarsin <yuanqingxiang233@163.com>
2022-08-20 01:39:36 +0530

    tulip: shims: Make shim for miwatermark.so
    Logs:
    
    vndksupport: Could not load /vendor/lib/hw/camera.sdm660.so from default namespace: dlopen failed: cannot locate symbol "_ZN4piex19GetPreviewImageDataEPNS_15StreamInterfaceEPNS_16PreviewImageDataE" referenced by "/vendor/lib/miwatermark.so"....
    
    Change-Id: I47fea33432692a7a76d71db91676cb3a58ac19c7
    Signed-off-by: Kujou Yuko <i@779.moe>
    Signed-off-by: Hemant Sachdeva <hemant.evolver@gmail.com>
    Signed-off-by: baibhab <fakebai34@gmail.com>

--- device.mk
+++ device.mk
@@ -397,7 +397,8 @@ PRODUCT_PACKAGES += \
 # Shims
 PRODUCT_PACKAGES += \
     libcamera_sdm660_shim \
-    libshim_dpmframework
+    libshim_dpmframework \
+    libwatermark_shim
 
 # Tetheroffload
 PRODUCT_PACKAGES += \
