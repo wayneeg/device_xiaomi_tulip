6242463cecd884b80f270f7feca91299347c90a5
Baibhab <60211800+baibhab34@users.noreply.github.com>
2022-09-05 03:03:05 +0530

    tulip: ship with gcam go
    Clone this https://github.com/baibhab34/packages_apps_Gcam -b go
    
    Signed-off-by: baibhab <fakebai34@gmail.com>

--- device.mk
+++ device.mk
@@ -144,6 +144,7 @@ PRODUCT_PACKAGES += \
     android.hardware.camera.provider@2.5:64 \
     android.hardware.camera.provider@2.6:64 \
     libdng_sdk.vendor \
+    GoogleCamera \
     vendor.qti.hardware.camera.device@1.0:64
 
 # Cgroup and task_profiles
