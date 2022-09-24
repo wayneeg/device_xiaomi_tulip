ff88c60504199b2b63fb592f1d81d67711304b42
baibhab <fakebai34@gmail.com>
2022-09-24 16:31:35 +0000

    tulip: don't build parts for now

--- device.mk
+++ device.mk
@@ -327,10 +327,6 @@ PRODUCT_PACKAGES += \
     libOmxVenc \
     libstagefrighthw
 
-# Parts
-PRODUCT_PACKAGES += \
-    XiaomiParts
-
 # Power
 PRODUCT_PACKAGES += \
     android.hardware.power-service.xiaomi-libperfmgr \
