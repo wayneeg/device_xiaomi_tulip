82689cbdc273548ff014e39154ad468ef5590ed0
Matheus Souza <33396386+matheucomth@users.noreply.github.com>
2022-12-12 07:46:43 -0300

    tulip: switch to common.mk

--- aosp_tulip.mk
+++ aosp_tulip.mk
@@ -9,7 +9,7 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 
 # Inherit some common aospOS stuff
-$(call inherit-product, vendor/aosp/config/common_full_phone.mk)
+$(call inherit-product, vendor/aosp/config/common.mk)
 
 # Inherit from tulip device
 $(call inherit-product, device/xiaomi/tulip/device.mk)
